6ee9b5bd91e5e8b7bd4388e36233fa6cc7ea9efc
Igor Stepanov <sigorilla@yandex-team.ru>
2016-04-27 21:29:05 +0300

    Index page

--- ml_training/urls.py
+++ ml_training/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
 ]
