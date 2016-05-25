651849799505e247b91dfc143608238777f8df61
Igor Stepanov <sigorilla@yandex-team.ru>
2016-05-25 22:47:18 +0300

    Change relationship for materials

--- competition/admin.py
+++ competition/admin.py
@@ -3,18 +3,23 @@ from django.utils.translation import ugettext as _
 from models import Competition, Material
 
 
+class MaterialInline(admin.StackedInline):
+    model = Material
+    extra = 2
+
+
 class CompetitionAdmin(admin.ModelAdmin):
     fieldsets = [
         (_('Create new competition'), {'fields': [
             'title', 'content', 'link',
             'start_date', 'finish_date', 'entry_deadline', 'submission_deadline',
-            'image', 'materials', 'active',
+            'image', 'active',
         ]})
     ]
+    inlines = [MaterialInline]
     list_display = ('title', 'start_date', 'active', 'was_published_recently')
     list_filter = ['pub_date', 'upd_date', 'active']
     search_fields = ['title', 'content']
     readonly_fields = ('pub_date', 'upd_date',)
 
 admin.site.register(Competition, CompetitionAdmin)
-admin.site.register(Material)
--- competition/models.py
+++ competition/models.py
@@ -37,24 +37,6 @@ class Link(models.Model):
     target = models.CharField(max_length=2, choices=TARGET_CHOICES, default='EM', verbose_name=_('target'))
 
 
-class Material(models.Model):
-
-    class Meta:
-        verbose_name = _('material')
-        verbose_name_plural = _('materials')
-
-    def __str__(self):
-        return self.title
-
-    def __unicode__(self):
-        return self.title
-
-    title = models.CharField(max_length=200, verbose_name=_('title'))
-    article = models.URLField(verbose_name=_('article link'), blank=True)
-    video = models.URLField(verbose_name=_('video link'), blank=True)
-    slides = models.URLField(verbose_name=_('slides link'), blank=True)
-
-
 class Competition(models.Model):
 
     class Meta:
@@ -88,11 +70,10 @@ class Competition(models.Model):
     entry_deadline = models.DateTimeField(default=timezone.now, verbose_name=_('entry deadline'))
     finish_date = models.DateTimeField(verbose_name=_('finish date'), default=timezone.now, blank=False)
     active = models.BooleanField(default=True, verbose_name=_('active?'))
-    image = models.TextField(verbose_name=_('url for logo'), default=DEFAULT_IMAGE)
+    image = models.URLField(verbose_name=_('url for logo'), default=DEFAULT_IMAGE)
     link = models.URLField(verbose_name=_('site link'), default='#', blank=False)
     links = models.ManyToManyField(Link, verbose_name=_('links'), blank=True)
     author = models.ForeignKey(User, verbose_name=_('author'), default=1)
-    materials = models.ManyToManyField(Material, verbose_name=_('materials'), blank=True)
 
     def was_published_recently(self):
         now = timezone.now()
@@ -100,3 +81,22 @@ class Competition(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = _('was published recently?')
+
+
+class Material(models.Model):
+
+    class Meta:
+        verbose_name = _('material')
+        verbose_name_plural = _('materials')
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
+
+    title = models.CharField(max_length=200, verbose_name=_('title'))
+    article = models.URLField(verbose_name=_('article link'), blank=True)
+    video = models.URLField(verbose_name=_('video link'), blank=True)
+    slides = models.URLField(verbose_name=_('slides link'), blank=True)
+    competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
